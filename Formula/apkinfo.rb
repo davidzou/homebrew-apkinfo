821a961edf714f9512d6f509403f6a59aee863fa
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-17 16:41:32 +0800

    update apkinfo.rb

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -1,8 +1,11 @@
-class Apkinfo < Tap
-  name "apkinfo"
+class Apkinfo < Formula
+  homepage ''
+  url 'https://github.com/davidzou/homebrew-apkinfo/apkinfohelp.tar.gz'
+  desc 'Apkinfo test'
+  sha256 '02ef6d07a029b3b2f9aad3ed6904f59f2311e0061753070cce972e25127da356'
 
-  def install(options = {})
-    prefix.install "apkinfo.sh"
+  def install
+    bin.install "apkinfohelp"
   end
 
   test do
