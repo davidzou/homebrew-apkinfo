68e31a78c1d05ced42877f4ad5562bd0c11d5018
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-17 16:43:10 +0800

    update apkinfo.rb add version

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -3,6 +3,7 @@ class Apkinfo < Formula
   url 'https://github.com/davidzou/homebrew-apkinfo/apkinfohelp.tar.gz'
   desc 'Apkinfo test'
   sha256 '02ef6d07a029b3b2f9aad3ed6904f59f2311e0061753070cce972e25127da356'
+  version '1.0'
 
   def install
     bin.install "apkinfohelp"
