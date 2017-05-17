3d4efe92ce29f0c308b74f7722604d00bf9aae8c
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-17 15:05:22 +0800

    update apkinfo.rb

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -1,7 +1,6 @@
 class Apkinfo < Tap
-  homepage "https://brew.sh/"
+  homepage "https://github.com/"
   sha256 "no_check"
-  desc "The command what apkinfo how to look up a apk file infomation."
   version "1.0.0"
 
   def install
