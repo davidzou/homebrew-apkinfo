78bbc0e48f230ddd88ed0a450c64886668a3f944
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-17 23:35:39 +0800

    update

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -6,7 +6,7 @@ class Apkinfo < Formula
   version '1.0'
 
   def install
-    bin.install "apkinfo.sh"
+    bin.install "src/apkinfo.sh"
     bin.install_symlink libexec/"apkinfo.sh" => "apkinfo"
   end
 
