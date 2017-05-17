06b81e4e2cfdd5f8deca5367b209126e3c44e9dc
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-17 19:32:39 +0800

    update

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -2,11 +2,11 @@ class Apkinfo < Formula
   homepage ''
   url 'https://github.com/davidzou/homebrew-apkinfo/raw/master/apkinfo.tar.gz'
   desc 'Apkinfo test'
-  sha256 'a3f4fe933d65c2b02add4d024bda8e7793f1565f25876c61809cb961bd864635'
+  sha256 '7e4d9b730476addcc236f88169ae7187213bfeda216af804b91bfb8a55327fc5'
   version '1.0'
 
   def install
-    libexec.install "apkinfo"
+    bin.install "apkinfo"
   end
 
   test do
