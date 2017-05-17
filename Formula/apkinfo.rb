f3e365670776620f8048fbb8adf9730fb251f6f4
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-17 13:49:10 +0800

    update apkinfo.rb

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -1,6 +1,6 @@
 class Apkinfo < Tap
   homepage "https://github.com/davidzou/homebrew-apkinfo"
-  url "https://github.com/davidzou/homebrew-apkinfo/apkinfo-1.0.0.tar.gz"
+#  url "https://github.com/davidzou/homebrew-apkinfo/apkinfo-1.0.0.tar.gz"
   sha256 "no_check"
   desc "The command what apkinfo how to look up a apk file infomation."
   version "1.0.0"
