87de1837d8d5ff04ba57334be137caea0e9661d9
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-19 01:09:54 +0800

    update version to auto replace

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -2,7 +2,7 @@ class Apkinfo < Formula
   homepage ''
   url 'https://github.com/davidzou/homebrew-apkinfo/blob/master/tarball/apkinfo.tar?raw=true'
   desc 'Apkinfo test'
-  sha256 'd1d7c2438d95fa0f91472b8329c1a88365240396b7d61aaadc9e6f1597459de5'
+  sha256 '3cab2d3fcc758e62d3d935809684e7695d6e0d35f995fe8c76753c0a26a80552'
   version '0.0.1'
 
   def install
--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#rm tarball/apkinfo.zip
-#zip -qj tarball/apkinfo.zip src/apkinfo LICENSE README.md CHANGELOG.md
-#zip tarball/apkinfo.zip src/tools
-#unzip -v tarball/apkinfo.zip
-#SHA256=`shasum -a 256 tarball/apkinfo.zip | cut -d " " -f1`
 
 VERSION=0.0.1
 BUILD_HOME=build/apkinfo/${VERSION}/
@@ -13,23 +8,31 @@ if [ -d build ] ; then
     rm -rf build
 fi
 
+# 创建构建目录build，拷贝打包文件
 mkdir -p ${BUILD_HOME}
 cp -r src/tools ${BUILD_HOME}
 cp src/apkinfo.sh ${BUILD_HOME}
 
+# 替换文件中的版本号
+sed -ig "s/APKINFO_VERSION=.*/APKINFO_VERSION=\"${VERSION}\"/g" ${BUILD_HOME}/apkinfo.sh
+sed -ig "s/#  version    :.*/#  version    : ${VERSION}/g" ${BUILD_HOME}/apkinfo.sh
+rm ${BUILD_HOME}/apkinfo.shg
+
+# 压缩资源包
 rm tarball/${FILE_NAME}
 tar -cvf tarball/${FILE_NAME} -C ${BUILD_HOME} apkinfo.sh tools/
 tar -zvtf tarball/${FILE_NAME}
-SHA256=`shasum -a 256 tarball/${FILE_NAME} | cut -d " " -f1`
 
+# 生成校验码
+SHA256=`shasum -a 256 tarball/${FILE_NAME} | cut -d " " -f1`
 echo "========================================================="
 echo ${SHA256}
 echo "========================================================="
 
+# 替换校验码
 sed -ig "s/sha256.*/sha256 '${SHA256}'/g" Formula/apkinfo.rb
-
+sed -ig "s/version.*/version '${VERSION}'/g" Formula/apkinfo.rb
 rm Formula/apkinfo.rbg
-rm -rf build
 
 #cat Formula/apkinfo.rb
 
@@ -40,6 +43,3 @@ rm -rf build
 #brew update
 #
 #brew install apkinfo
-
-
-#/usr/local/Cellar/apkinfo/1.0/bin/
