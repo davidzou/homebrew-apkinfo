ccd10877d48dc94f5eca9de581ea612b3c0ea439
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-18 18:16:53 +0800

    update rb

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -1,6 +1,6 @@
 class Apkinfo < Formula
   homepage ''
-  url 'https://github.com/davidzou/homebrew-apkinfo/blob/master/tarball/apkinfo.zip?raw=true'
+  url 'https://github.com/davidzou/homebrew-apkinfo/blob/master/tarball/apkinfo.tar.gz?raw=true'
   desc 'Apkinfo test'
   sha256 'b2a6fcc994e98bb10b037da55b65b7c04a6afaec390654361ee0fc2a37ff69a4'
   version '0.0.1'
