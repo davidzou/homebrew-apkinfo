49502d969043b629c03af410e77bf11c0584f687
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-26 00:15:12 +0800

    update all version to 0.0.2

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -2,7 +2,7 @@ class Apkinfo < Formula
   homepage ''
   url 'https://github.com/davidzou/homebrew-apkinfo/blob/master/tarball/apkinfo.tar?raw=true'
   desc 'Apkinfo test'
-  sha256 '86918e00c7cf32295bd7f3e68a55580f8c735b08eb80aac4c6444472fc8453e5'
+  sha256 'a1558d66f22993a01b722cb500e8695c16f5cec6c1fe7f22292bbe0c72618bd5'
   version '0.0.2'
 
   def install
--- build.sh
+++ build.sh
@@ -17,14 +17,20 @@ cp src/apkinfo.1 ${BUILD_HOME}
 # 替换文件中的版本号
 sed -ig "s/APKINFO_VERSION=.*/APKINFO_VERSION=\"${VERSION}\"/g" ${BUILD_HOME}/apkinfo.sh
 sed -ig "s/#  version.*/#  version    : ${VERSION}/g" ${BUILD_HOME}/apkinfo.sh
-sed -ig "s/#  version.*/#  version    : ${VERSION}/g" ${BUILD_HOME}/apkinfo_options_help.sh
+sed -ig "s/#  version.*/#  version    : ${VERSION}/g" ${BUILD_HOME}/tools/apkinfo_options_help.sh
 sed -ig "s/Version :.*/Version :       ${VERSION}/g" ${BUILD_HOME}/tools/apkinfo_options_help.txt
 sed -ig "s/\.TH man 1 \".*/\.TH man 1 \"10 May 2017\" \"${VERSION}\" \"apkinfo man page\"/g" ${BUILD_HOME}/apkinfo.1
+sed -ig "s/#  version.*/#  version    : ${VERSION}/g" ${BUILD_HOME}/tools/table.sh
+sed -ig "s/#  version.*/#  version    : ${VERSION}/g" ${BUILD_HOME}/tools/logger.sh
+sed -ig "s/#  version.*/#  version    : ${VERSION}/g" ${BUILD_HOME}/tools/apkinfo_i18n.sh
 
 rm ${BUILD_HOME}/apkinfo.shg
 rm ${BUILD_HOME}/apkinfo.1g
 rm ${BUILD_HOME}/tools/apkinfo_options_help.txtg
 rm ${BUILD_HOME}/tools/apkinfo_options_help.shg
+rm ${BUILD_HOME}/tools/table.shg
+rm ${BUILD_HOME}/tools/logger.shg
+rm ${BUILD_HOME}/tools/apkinfo_i18n.shg
 
 # 压缩资源包
 rm tarball/${FILE_NAME}
