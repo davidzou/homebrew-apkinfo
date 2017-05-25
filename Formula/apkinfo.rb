c5ef7208f959bc4bd2200955c2505ebc92d0d85b
zouzhangwei <zouzhangwei@hujiang.com>
2017-05-25 23:58:23 +0800

    update to v0.0.2

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -2,8 +2,8 @@ class Apkinfo < Formula
   homepage ''
   url 'https://github.com/davidzou/homebrew-apkinfo/blob/master/tarball/apkinfo.tar?raw=true'
   desc 'Apkinfo test'
-  sha256 '8b804cddcf02f543d0647a82350e7310c29a452a3bb03cd43444910e4b732155'
-  version '0.0.1'
+  sha256 '86918e00c7cf32295bd7f3e68a55580f8c735b08eb80aac4c6444472fc8453e5'
+  version '0.0.2'
 
   def install
     bin.install "apkinfo.sh" => "apkinfo"
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.0.1
+VERSION=0.0.2
 BUILD_HOME=build/apkinfo/${VERSION}/
 FILE_NAME=apkinfo.tar
 
