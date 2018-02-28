9f493a9d3a63e2a15bc0c1a7eda2a247909f9fc1
zouzhangwei <zouzhangwei@hujiang.com>
2018-02-28 13:35:09 +0800

    fix descript

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -1,7 +1,7 @@
 class Apkinfo < Formula
   homepage ''
   url 'https://github.com/davidzou/homebrew-apkinfo/blob/master/tarball/apkinfo_0.0.5/apkinfo-command-v0.0.5.tar?raw=true'
-  desc 'A command what to look up a apk info, that include meta-data applicationid version '0.0.5'
+  desc 'A command what to look up a apk info, that include meta-data applicationid version 0.0.5'
   sha256 '084b6a08e0776b21479ad325a331374b713e41a75ecdc595a11782bc42a5a38f'
   version '0.0.5'
 
